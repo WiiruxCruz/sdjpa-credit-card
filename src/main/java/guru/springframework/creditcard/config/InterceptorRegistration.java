4f83d8de57debb6d9f12977b0d357860b65fce3c
WiiruxCruz <hialf.92@gmail.com>
2023-02-03 13:50:23 -0600

    Create Hibernate interceptor

---
+++ src/main/java/guru/springframework/creditcard/config/InterceptorRegistration.java
@@ -0,0 +1,23 @@
+package guru.springframework.creditcard.config;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+import guru.springframework.creditcard.interceptors.EncryptionInterceptor;
+
+@Configuration
+public class InterceptorRegistration implements HibernatePropertiesCustomizer{
+	
+	@Autowired
+	EncryptionInterceptor interceptor;
+
+	@Override
+	public void customize(Map<String, Object> hibernateProperties) {
+		// TODO Auto-generated method stub
+		hibernateProperties.put("hibernate.session_factory.interceptor", interceptor);
+	}
+
+}
